a19e5f9dc3c7ccadcca4f6407e6da3a2823e5945
zhangyixing.zyx <zhangyixing.zyx@antgroup.com>
2023-09-20 11:30:20 +0800

    chore: add component AutoInsightVis

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-// const root = document.getElementById('root') as HTMLElement;
+import AutoInsightVis from './AutoInsightVis';
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <div id="APP">
         <div id="control_bar" className="view">
           <h2>Control</h2>
-          
         </div>
         <div id = "bottom">
               <div id = "visualization" className="view">
                 <h2>Text Vis</h2>
-              
+              <AutoInsightVis></AutoInsightVis>
               </div>
             <div id = "design" className="view">
               <h2>Design</h2>
-              
-              {/* <AutoInsightVis></AutoInsightVis> */}
             </div>
         </div>
     </div>
-    
-    {/* <AutoInsight></AutoInsight>
-    <Learn_ts></Learn_ts>
-    <AutoInsightVis></AutoInsightVis> */}
   </React.StrictMode>,
   document.getElementById('root')
 );

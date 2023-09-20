70df2eebfa2a91810f23a0393f25b01f4cf3794b
zhangyixing.zyx <zhangyixing.zyx@antgroup.com>
2023-09-20 11:13:19 +0800

    chore: set project layout

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = document.getElementById('root') as HTMLElement;
+// const root = document.getElementById('root') as HTMLElement;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div id="APP">
+        <div id="control_bar" className="view">
+          <h2>Control</h2>
+          
+        </div>
+        <div id = "bottom">
+              <div id = "visualization" className="view">
+                <h2>Text Vis</h2>
+              
+              </div>
+            <div id = "design" className="view">
+              <h2>Design</h2>
+              
+              {/* <AutoInsightVis></AutoInsightVis> */}
+            </div>
+        </div>
+    </div>
+    
+    {/* <AutoInsight></AutoInsight>
+    <Learn_ts></Learn_ts>
+    <AutoInsightVis></AutoInsightVis> */}
   </React.StrictMode>,
-  root
+  document.getElementById('root')
 );
-
-reportWebVitals();

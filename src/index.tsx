d206bd88a4e29f3d80a72f7e21bc0bc131ce0791
zhangyixing.zyx <zhangyixing.zyx@antgroup.com>
2023-10-13 15:46:50 +0800

    chore: create project

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = document.getElementById('root') as HTMLElement;
-
+import AutoInsightVis from './AutoInsightVis';
+import ControlBar from './ControlBar';
+import InsightCards from "./components/InsightCards";
+import './index.css'
+import VisualText from './components/VisualText';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div id="APP">
+        <div id="control_bar" className="view">
+          <h2>Control</h2>
+          <ControlBar></ControlBar>
+        </div>
+        <div id = "bottom">
+              <div id = "visualization" className="view">
+                <h2>Text Vis</h2>
+              {/* <AutoInsightVis></AutoInsightVis> */}
+              {/* <VisualText></VisualText> */}
+              <InsightCards></InsightCards>
+              </div>
+            <div id = "design" className="view">
+              <h2>Design</h2>
+            </div>
+        </div>
+    </div>
   </React.StrictMode>,
-  root
+  document.getElementById('root')
 );
-
-reportWebVitals();

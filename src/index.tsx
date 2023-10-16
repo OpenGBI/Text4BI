8b6c30c83d0dd2ad117e14823c30a18fb859ee64
zhangyixing.zyx <zhangyixing.zyx@antgroup.com>
2023-10-16 11:21:37 +0800

    feat: use redux for state management

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,19 @@ import reportWebVitals from './reportWebVitals';
 const root = document.getElementById('root') as HTMLElement;
 
 ReactDOM.render(
+<<<<<<< HEAD
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 	root
+=======
+  <React.StrictMode>
+    <div id="APP">
+        
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+>>>>>>> 5232ddb (feat: use redux for state management)
 );
 
 reportWebVitals();

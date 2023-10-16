43272d783489bfbab6741aeef1fd64b63e12228e
zhangyixing.zyx <zhangyixing.zyx@antgroup.com>
2023-10-16 11:21:37 +0800

    feat: use redux for state management

--- src/index.tsx
+++ src/index.tsx
@@ -15,17 +15,7 @@ import './index.css'
 ReactDOM.render(
   <React.StrictMode>
     <div id="APP">
-        <div id="control_bar" className="view">
-          <h2>Control</h2>
-        </div>
-        <div id = "bottom">
-              <div id = "visualization" className="view">
-                <h2>Text Vis</h2>
-              </div>
-            <div id = "design" className="view">
-              <h2>Design</h2>
-            </div>
-        </div>
+        
     </div>
 >>>>>>> d1f26f1 (feat: use redux for state management)
   </React.StrictMode>,

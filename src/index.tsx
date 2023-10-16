74660f0435c48d568160deb58dedfb8d57c620dc
zhangyixing.zyx <zhangyixing.zyx@antgroup.com>
2023-10-16 17:38:22 +0800

    refactor: use eslint to refactor code, rafactor redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+<<<<<<< HEAD
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -20,6 +21,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 >>>>>>> 5232ddb (feat: use redux for state management)
+=======
+ReactDOM.render(
+	<React.StrictMode>
+		<div id="APP"></div>
+	</React.StrictMode>,
+	document.getElementById('root')
+>>>>>>> 205642f (refactor: use eslint to refactor code, rafactor redux)
 );
 
 reportWebVitals();
--- src/store.tsx
+++ src/store.tsx
@@ -1,19 +1,15 @@
-import { createStore,combineReducers, applyMiddleware } from "redux";
-import { systemReducer } from "./reducer/systemReducer";
-import { GlobalSettingReducer } from "./reducer/globalReducer";
-import { loggerMiddleware } from "./middlewares/loggerMiddleware";
-import { ErrorMiddleware } from "./middlewares/errorMiddleware";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { systemReducer } from './reducer/systemReducer';
+import { GlobalSettingReducer } from './reducer/globalReducer';
+import { loggerMiddleware } from './middlewares/loggerMiddleware';
+import { ErrorMiddleware } from './middlewares/errorMiddleware';
 
-const rootReducer = combineReducers(
-    {
-        system:systemReducer,
-        globalSetting:GlobalSettingReducer
+const rootReducer = combineReducers({
+	system: systemReducer,
+	globalSetting: GlobalSettingReducer,
+});
+export type AppState = ReturnType<typeof rootReducer>;
 
-    }
-)
-export type AppState = ReturnType<typeof rootReducer>
+const MiddlewareEnhancer = applyMiddleware(loggerMiddleware, ErrorMiddleware);
 
-const MiddlewareEnhancer = applyMiddleware(loggerMiddleware, ErrorMiddleware)
-
-export const store = createStore(rootReducer, MiddlewareEnhancer)
-
+export const store = createStore(rootReducer, MiddlewareEnhancer);

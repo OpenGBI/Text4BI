a1a04b54ec13ea8c025d93530fbda1cb68eb125e
zhangyixing.zyx <zhangyixing.zyx@antgroup.com>
2023-10-16 11:09:07 +0800

    feat: use redux for state management

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,6 +10,24 @@ const root = document.getElementById('root') as HTMLElement;
 ReactDOM.render(
   <React.StrictMode>
     <App />
+=======
+import './index.css'
+ReactDOM.render(
+  <React.StrictMode>
+    <div id="APP">
+        <div id="control_bar" className="view">
+          <h2>Control</h2>
+        </div>
+        <div id = "bottom">
+              <div id = "visualization" className="view">
+                <h2>Text Vis</h2>
+              </div>
+            <div id = "design" className="view">
+              <h2>Design</h2>
+            </div>
+        </div>
+    </div>
+>>>>>>> d1f26f1 (feat: use redux for state management)
   </React.StrictMode>,
   root
 );
---
+++ src/types.tsx
@@ -0,0 +1,25 @@
+export type storeType = {
+    color:string,
+    font:string,
+}
+
+export type systemStateType = {
+    
+        "dataset":string,
+        "showBigGraph":boolean,
+        "showSparkLine":boolean,
+        "selectedCard":string[]
+      
+}
+export type GlobalSettingStateType = {
+    "color":string,
+    "boldness":boolean,
+    "underline":boolean,
+    "fontsize":string,
+    "backgroundColor":string,
+    "bulletPoint":boolean,
+    "lineHeight":number,
+    "sparkLinePosition":string,//上下左右
+    "aspectRatio":string,//​短（1:1）、中（4:3）、长（16:9） 
+}
+

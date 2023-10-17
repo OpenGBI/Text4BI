f47105c9bbc9f7416291bb19279c8ebfbf476563
zhangyixing.zyx <zhangyixing.zyx@antgroup.com>
2023-10-17 14:21:09 +0800

    refactor: Resolve code conflicts

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = document.getElementById('root') as HTMLElement;
 
 ReactDOM.render(
-<<<<<<< HEAD
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 	root
-=======
-  <React.StrictMode>
-    <div id="APP">
-        
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
->>>>>>> 5232ddb (feat: use redux for state management)
-=======
-ReactDOM.render(
-	<React.StrictMode>
-		<div id="APP"></div>
-	</React.StrictMode>,
-	document.getElementById('root')
->>>>>>> 205642f (refactor: use eslint to refactor code, rafactor redux)
 );
 
 reportWebVitals();

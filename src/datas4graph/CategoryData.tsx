8f69e05bd02dd9efd13297b64bc1b34f98cded45
zhangyixing.zyx <zhangyixing.zyx@antgroup.com>
2023-11-29 17:22:25 +0800

    chore: before merge

---
+++ src/datas4graph/CategoryData.tsx
@@ -0,0 +1,11 @@
+export const Category11 = {
+  data: [
+    { category: 'New York City', value: 256368.161 },
+    { category: 'Los Angeles', value: 175851.34100000001 },
+    { category: 'Manila', value: 120886.9485 },
+    { category: 'Seattle', value: 119540.742 },
+    { category: 'San Francisco', value: 112669.092 },
+    { category: 'Philadelphia', value: 109077.013 },
+    { category: 'Sydney', value: 101945.517 },
+  ],
+}

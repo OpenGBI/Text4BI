17608ab5aaed26dba2be3b255fbc93a3f2a9bd56
zhangyixing.zyx <zhangyixing.zyx@antgroup.com>
2024-03-17 14:36:11 +0800

    feat: merge reset entity type

--- back/funcs/TemporalPeriodly.py
+++ back/funcs/TemporalPeriodly.py
@@ -35,7 +35,6 @@ def changeTemporalPeriodly(timeSegmentationCondition):
    
     # print(target["paragraph"][0]["phrases"][-1]["value"].split(" ")[:-1].join(" "))
     #修改大图数据
-<<<<<<< HEAD
     target["paragraph"][-2]["metadata"]["detail"]=new_data["data"]
     # #修改bullet point
     # topics = [paragraph for paragraph in target['paragraph'] if paragraph['type'] == 'normal']
@@ -70,24 +69,6 @@ def changeTemporalPeriodly(timeSegmentationCondition):
     #             phrase["value"]=str(round(abnormal_data["predict"],2))
     #             phrase["metadata"]["origin"]=round(abnormal_data["predict"],2)
     #             break
-=======
-    target["paragraph"][-2]["metadata"]["detail"]=export_json["data"]
-    target["paragraph"][-2]["metadata"]["tagData"]=export_json["tagData"]
-    #修改bullet point
-    topics = [paragraph for paragraph in target['paragraph'] if paragraph['type'] == 'bullet']
-    for i in range(0,len(topics)):
-        for phrase in topics[i]['phrases']:
-            if phrase.get('metadata', {}).get('entityType') == 'metric_names':
-                phrase["value"]=export_json["data"][i]["category"]
-                break
-        for phrase in topics[i]['phrases']:
-            if phrase.get('metadata', {}).get('entityType') == 'metric_value':
-                phrase["value"]=str(round(export_json["data"][i]["value"]*100,2))+"%"
-                phrase["metadata"]["origin"]=export_json["data"][i]["value"]
-                
-                phrase["metadata"]["detail"]=[export_json["data"][i]["value"],1-export_json["data"][i]["value"]]
-                break
->>>>>>> refs/remotes/origin/dev-all
 
     
     # for item in data:
